b2d2bd2f6803bf2cfd26207ba7e948e158c6200a
JakeRoberts99 <jake.s.roberts99@gmail.com>
2022-02-08 00:12:46 -0700

    added the variables needed for the assignment

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,4 +1,9 @@
 // add vars to get the element needed for the assignment
+var searchBtnElement = document.querySelector('.btn')
+var currentCityElement = document.getElementById('#current-city')
+var currentWeatherElement = document.getElementById('#current-weather')
+var searchCityElement = document.getElementById('#search-city')
+var fiveDayElement = document.getElementById('#five-day')
 // make a function for the api call 
 // make a function to call the data needed 
 // add event listeners for the btns
