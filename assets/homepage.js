0146ca32821602b6589e6848491cab48ffef5bb6
JakeRoberts99 <jake.s.roberts99@gmail.com>
2022-02-08 00:03:44 -0700

    added psuedo code to make the process easier

---
+++ assets/homepage.js
@@ -0,0 +1,8 @@
+// add vars to get the element needed for the assignment
+// make a function for the api call 
+// make a function to call the data needed 
+// add event listeners for the btns
+// save cities searched 
+// get the current day 
+// display the weather of the city searched 
+// show five day forecast 
